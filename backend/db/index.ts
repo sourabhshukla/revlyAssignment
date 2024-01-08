7b7b803a6d2fd10091155abafc9b4124cf090068
sourabhshukla <sourabhshukla9893502147@gmail.com>
2024-01-08 08:00:48 +0530

    removed unused info

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,13 +1,5 @@
 import { Pool } from "pg";
 
-// const pool = new Pool({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "revlyAssignment",
-//   password: "sHUKLA@1999",
-//   port: 5432,
-// });
-
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
